2af9cb8e0055b741f26338347ccb4828cd10f707
ThanhZung179 <dung1792000@gmail.com>
2023-06-22 17:07:19 +0700

    Done Modal

--- src/components/ModalAddCard.jsx
+++ src/components/ModalAddCard.jsx
@@ -1,4 +1,4 @@
-import { Modal, Form, Input, Select, Space } from 'antd';
+import { Modal, Form, Input, Select, Space, Avatar } from 'antd';
 import { useForm } from 'antd/es/form/Form';
 
 const { TextArea } = Input;
@@ -6,13 +6,7 @@ const { TextArea } = Input;
 function ModalAddCard({ isOpenModalAddCard, handleCloseModalAddCard, handleAddTodo }) {
 
   const [form] = useForm();
-  const options = [];
-  for (let i = 10; i < 36; i++) {
-    options.push({
-      value: i.toString(36) + i,
-      label: i.toString(36) + i,
-    });
-  }
+  
   const handleMemberSelectedChange = (value) => {
     console.log(`Selected: ${value}`);
   };
@@ -25,6 +19,10 @@ function ModalAddCard({ isOpenModalAddCard, handleCloseModalAddCard, handleAddTo
 
   const onFinishFailed = (errorInfo) => {
     console.log('Failed:', errorInfo);
+  };
+
+  const handleStatusChange = (value) => {
+    console.log(`selected ${value}`);
   };
 
   return (
@@ -45,7 +43,7 @@ function ModalAddCard({ isOpenModalAddCard, handleCloseModalAddCard, handleAddTo
         >
           <Form.Item
             label="Title"
-            name="title"
+            name="title" 
             rules={[
               {
                 required: true,
@@ -69,7 +67,8 @@ function ModalAddCard({ isOpenModalAddCard, handleCloseModalAddCard, handleAddTo
             <TextArea rows={4} />
           </Form.Item>
           
-          <Form.Item name="gender" label="Member" rules={[{ required: true }]}>
+        <Form.Item name="Member" label="Member" rules={[{ required: true,
+                message: 'Please input your member!', }]}>
           <Space
         direction="vertical"
         style={{
@@ -84,12 +83,69 @@ function ModalAddCard({ isOpenModalAddCard, handleCloseModalAddCard, handleAddTo
           style={{
             width: '100%',
           }}
-          options={options}
+          options={
+            [
+              {
+                value: 'zung',
+                label:  (
+                  <>
+                  <Space direction="vertical" size={16}>
+                    <Space wrap size={16}>
+                      <Avatar src='https://img.freepik.com/free-photo/stylish-confident-businesswoman-smiling_176420-19466.jpg?w=2000' />
+                     </Space>
+                    </Space>
+                  <span> Nguyen Thanh Zung </span>
+                  </>
+                  )
+                ,
+              },
+              {
+                value: 'lucy',
+                label: (
+                  <>
+                  <Space direction="vertical" size={16}>
+                    <Space wrap size={16}>
+                      <Avatar src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxG7Yll-Mqdj3Ce_9XfWDQ3qqvNTpEX82IeQ&usqp=CAU' />
+                     </Space>
+                    </Space>
+                  <span> Lucy </span>
+                  </>
+                  ),
+              },
+              {
+                value: 'tom',
+                label: (
+                  <>
+                  <Space direction="vertical" size={16}>
+                    <Space wrap size={16}>
+                      <Avatar src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvmI2RMfPSRXyXnYQYtF3tdjvwF4x2HnZH29Q-MjH0i3Ly-UAahLK3rq_mIEwcomCFNxk&usqp=CAU' />
+                     </Space>
+                    </Space>
+                  <span> Tom </span>
+                  </>
+                  ),
+              },
+            ]
+          }
         />
         
       </Space>
       </Form.Item>
 
+      <Form.Item 
+      name="Status" 
+      label="Status" >
+      <Select
+      defaultValue="New"
+      style={{ width: 120 }}
+      onChange={handleStatusChange}
+      options={[
+        { value: 'New', label: 'New' },
+        { value: 'In process', label: 'In process' },
+        { value: 'Done', label: 'Done' },
+      ]}
+    />
+    </Form.Item>
 
 
         </Form>
