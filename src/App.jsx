de30fa104979dd1a329aca80cab485ed93a1d60e
ThanhZung179 <dung1792000@gmail.com>
2023-06-24 14:27:07 +0700

    done add another list button

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import { Card, Button, Tooltip } from 'antd'
-import { PlusOutlined, DeleteOutlined } from '@ant-design/icons'
-import { useState } from 'react'
-import ModalAddCard from './components/ModalAddCard'
-import CardItem from './components/CardItem'
+import { Card, Button, Tooltip } from 'antd';
+import { PlusOutlined, DeleteOutlined } from '@ant-design/icons';
+import { useState } from 'react';
+import ModalAddCard from './components/ModalAddCard';
+import CardItem from './components/CardItem';
 
-function App() {
-
-  const [todos, setTodos] = useState([])
-
+export default function App() {
+  const [todos, setTodos] = useState([]);
   const [isOpenModalAddCard, setIsOpenModalAddCard] = useState(false);
+  const [listCount, setListCount] = useState(2);
 
   function handleOpenModalAddCard() {
     setIsOpenModalAddCard(true);
@@ -18,15 +17,20 @@ function App() {
     setIsOpenModalAddCard(false);
   }
 
-  function handleAddTodo(values) {
-    const todoItemn = {
+  function handleAddTodoList(values) {
+    const todoItem = {
       id: Math.random(),
       title: values.title,
       description: values.description,
       member: values.member,
-      status: values.status
-    }
-    setTodos([...todos, todoItemn]); // add item
+      status: values.status,
+    };
+
+    setTodos([...todos, todoItem]);
+  }
+
+  function handleAddList() {
+    setListCount((prevCount) => prevCount + 1);
   }
 
   return (
@@ -35,72 +39,52 @@ function App() {
         <div className="header_logo"></div>
         <div className="header_right">
           <div className="header_avatar">
-            <img src="https://th.bing.com/th/id/OIP.JcN63wd82WxNh9duWs6PXwHaLH?pid=ImgDet&rs=1" />
+            <img src="https://th.bing.com/th/id/OIP.JcN63wd82WxNh9duWs6PXwHaLH?pid=ImgDet&rs=1" alt="Avatar" />
           </div>
         </div>
       </div>
 
       <main>
         <div className="container">
-          <Card
-            title="List 1"
-            extra={
-              <>
-                <div style={{ display: 'flex', gap: '10px' }}>
-                  <Tooltip placement="top" title="Add a card">
-                    <Button shape="circle" icon={<PlusOutlined />} onClick={handleOpenModalAddCard}></Button>
-                  </Tooltip>
-                  <Tooltip placement="top" title="Delete this list">
-                    <Button shape="circle" icon={<DeleteOutlined />} />
-                  </Tooltip>
-                </div>
-              </>
-            }
-            style={{
-              width: 300,
-            }}
-            className="cardList"
-          >
-            {todos.map((todo) => (
-              <CardItem key={todo.id} todo={todo} />
-            ))}
-          </Card>
-          <Card
-            title="List 2"
-            extra={
-              <>
-                <div style={{ display: 'flex', gap: '10px' }}>
-                  <Tooltip placement="top" title="Add a card">
-                    <Button shape="circle" icon={<PlusOutlined />} onClick={handleOpenModalAddCard}></Button>
-                  </Tooltip>
-                  <Tooltip placement="top" title="Delete this list">
-                    <Button shape="circle" icon={<DeleteOutlined />} />
-                  </Tooltip>
-                </div>
-              </>
-            }
-            style={{
-              width: 300,
-            }}
-            className="cardList"
-          >
-            {todos.map((todo) => (
-              <CardItem key={todo.id} todo={todo} />
-            ))}
-          </Card>
+          {Array.from({ length: listCount }, (_, index) => (
+            <Card
+              key={index}
+              title={`List ${index + 1}`}
+              extra={
+                <>
+                  <div style={{ display: 'flex', gap: '10px' }}>
+                    <Tooltip placement="top" title="Add a card">
+                      <Button shape="circle" icon={<PlusOutlined />} onClick={handleOpenModalAddCard} />
+                    </Tooltip>
+                    <Tooltip placement="top" title="Delete this list">
+                      <Button shape="circle" icon={<DeleteOutlined />} />
+                    </Tooltip>
+                  </div>
+                </>
+              }
+              style={{
+                width: 300,
+              }}
+              className="cardList"
+            >
+              {todos.map((todo) => (
+                <CardItem key={todo.id} todo={todo} />
+              ))}
+            </Card>
+          ))}
+
+          <Button onClick={handleAddList} icon={<PlusOutlined />} >Add another List</Button>
+
         </div>
       </main>
 
+
+
       <ModalAddCard
         isOpenModalAddCard={isOpenModalAddCard}
-        handleAddTodo={handleAddTodo}
+        handleAddTodo={handleAddTodoList}
         handleCloseModalAddCard={handleCloseModalAddCard}
       />
-
-
-
     </>
-  )
+  );
 }
-
-export default App
